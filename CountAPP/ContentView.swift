2c2c951ca741de888caec6f15451211740c57a56
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2020-10-23 20:20:46 +0900

    Initial Commit

---
+++ CountAPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CountAPP
+//
+//  Created by user on 2020/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CountAPP/CountAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountAPPApp.swift
+//  CountAPP
+//
+//  Created by user on 2020/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CountAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
